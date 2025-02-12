8336617475c326d4bd6826f8e73ae2437f57cadd
Chirayu Sahu <81841434+ChirayuSahu@users.noreply.github.com>
2025-02-12 22:23:56 +0530

    Add files via upload

--- app/firebase/config.tsx
+++ app/firebase/config.tsx
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore, doc, getDoc, setDoc, updateDoc, deleteDoc, collection, addDoc, getDocs, where, query} from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAUPQf8GYES7n2ZQMx5Cosa1bDXvxiwVqk",
-    authDomain: "expense-tracker-701df.firebaseapp.com",
-    databaseURL: "https://expense-tracker-701df-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "expense-tracker-701df",
-    storageBucket: "expense-tracker-701df.firebasestorage.app",
-    messagingSenderId: "13114654172",
-    appId: "1:13114654172:web:d845d776f058ba21cce319"
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_DB_URL,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID
   };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
